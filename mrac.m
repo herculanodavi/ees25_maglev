9f63a64563dc531670078a474d64376f0fffe2ce
apreciavel <herculanodavi@aluno.ita.br>
2017-09-14 12:08:24 -0300

    inserindo sistema de referencia

--- mrac.m
+++ mrac.m
@@ -1,5 +1,6 @@
 % variaveis simbolicas
 syms s r;
+syms xi wn;
 syms a b c;
 syms kp kd ki;
 
@@ -9,26 +10,35 @@ gammaKd = 1;
 gammaKi = 1;
 a_exp = 1;
 b_exp = 1;
-c_exp = 1;
+c_exp = 1;  
+
+% requisitos desejados
+ts = 1;
+mn = 1;
+xi_exp = 1;
+wn_exp = 1;
 
 % funcao de transferencia da planta
 %G(s) = 0.004813/(s^2 + 1.25 *s + 6.031);
-G(s, a, b, c) = a/(s^2 + b*s + c);
+G = a/(s^2 + b*s + c);
+
+% funcao de transferencia da planta desejada
+G_m = 1/(s^2 + 2*wn*xi*s + wn^2);
 
 % funcao de transferencia do sistema com controlador
-G_c(s, kp, ki, kd, a, b, c) = (kp + ki/s)/(1/G(s, a, b, c) + kp + ki/s + s*kd);
+G_c = (kp + ki/s)/(1/G + kp + ki/s + s*kd);
 
 % erro de adaptação
-y_p(r, s, kp, ki, kd, a, b, c) = G_c(s, kp, ki, kd, a, b, c) * r;
-e(r, s, kp, ki, kd, a, b, c) = r - y_p(r, s, kp, ki, kd, a, b, c);
-epsilon(r, s, kp, ki, kd, a, b, c) = G_c(s, kp, ki, kd, a, b, c)*r - G(s, a, b, c)*r;
+y_p     = G_c * r;
+e       = r - y_p;
+epsilon = y_p - G_m*r;
 
 % obtendo derivadas com respeito a kp, ki, kd
-de_dkp(r, s, kp, ki, kd, a, b, c) = diff(epsilon(r, s, kp, ki, kd, a, b, c), kp);
-de_dki(r, s, kp, ki, kd, a, b, c) = diff(epsilon(r, s, kp, ki, kd, a, b, c), ki);
-de_dkd(r, s, kp, ki, kd, a, b, c) = diff(epsilon(r, s, kp, ki, kd, a, b, c), kd);
+de_dkp = diff(epsilon, kp);
+de_dki = diff(epsilon, ki);
+de_dkd = diff(epsilon, kd);
 
 % aplicando na regra do mit (dTheta/dt = dJ/dTheta)
-dkp_dt = - gammaKp * epsilon(r, s, kp, ki, kd, a, b, c) * de_dkp(r, s, kp, ki, kd, a, b, c);
-dkd_dt = - gammaKd * epsilon(r, s, kp, ki, kd, a, b, c) * de_dkd(r, s, kp, ki, kd, a, b, c);
-dki_dt = - gammaKi * epsilon(r, s, kp, ki, kd, a, b, c) * de_dki(r, s, kp, ki, kd, a, b, c);
+dkp_dt = - gammaKp * epsilon * de_dkp;
+dki_dt = - gammaKi * epsilon * de_dki;  
+dkd_dt = - gammaKd * epsilon * de_dkd;
