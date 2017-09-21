60686b8713b5d22925b11efd4df87f64db0e00cf
apreciavel <herculanodavi@aluno.ita.br>
2017-09-21 12:35:01 -0300

    sistema atualizado, ainda ha bug no emiraque

--- mrac.m
+++ mrac.m
@@ -22,12 +22,13 @@ wn_exp = 1;
 
 % funcao de transferencia da planta
 %G(s) = 0.004813/(s^2 + 1.25 *s + 6.031);
-G = a/(s^2 + b*s + c);
+syms gp;
+G(s) = a/(s^2 + b*s + c);
 
 % funcao de transferencia da planta desejada
 G_m = 1/(s^2 + 2*wn*xi*s + wn^2);
 % funcao de transferencia do sistema com controlador
-G_c = (kp + ki/s)/(1/G + kp + ki/s + s*kd);  %1/G
+G_c = (kp + ki/s)/(1/gp + kp + ki/s + s*kd);  %1/G
 
 % erro de adaptação
 y_p     = G_c * r;
@@ -38,7 +39,9 @@ epsilon = y_p - G_m*r;
 de_dkp = diff(epsilon, kp);
 de_dki = diff(epsilon, ki);
 de_dkd = diff(epsilon, kd);
-
+de_dkp = simplify(de_dkp);
+de_dki = simplify(de_dki);
+de_dkd = simplify(de_dkd);
 % aplicando na regra do mit (dTheta/dt = dJ/dTheta)
 dkp_dt = - gammaKp * epsilon * de_dkp;
 dki_dt = - gammaKi * epsilon * de_dki;  
